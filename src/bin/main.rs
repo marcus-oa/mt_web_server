a295d314f7b6053226b3ab3f1fd730ea2bbf2fe1
marcus_oa <marcus.official1@gmail.com>
2021-03-03 18:53:54 +0000

    Multithreading implementation added

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,24 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use mt_web_server::ThreadPool;
 
 fn main() {
     // listen for incoming TCP streams at address 127.0.0.1:7878
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // hypothetical syntax for creating a ThreadPool
+    let pool = ThreadPool::new(4);
+
     // iterate over received connections on address 127.0.0.1:7878
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream)
+        })
     }
 }
 
@@ -25,8 +33,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     // b to code as bytes
     let get  = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
